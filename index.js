7e039ebe6b4e04fb64bd5a53169afc47113b6fba
JD <moosay50000@gmail.com>
2024-01-19 12:12:53 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+gsap.from("#clabs",{
+    y:70,
+    duration:1,
+    delay:7
+     
+}) 
+gsap.from("#about h1 ",{
+    x:70,
+    duration:1,
+    delay:3,
+    opacity:0,
+      
+}) 
+gsap.from("#about p ",{
+    x:70,
+    duration:1,
+    delay:5,
+    opacity:0,
+    stagger:1
+     
+}) 
+gsap.from("#clabOne",{
+    x:70,
+    duration:1,
+    delay:7,
+    opacity:0
+})
+gsap.from("#clabTwo",{
+    x:70,
+    duration:1,
+    delay:8,
+    opacity:0
+})
+gsap.from("#clabThree",{
+    x:70,
+    duration:1,
+    delay:9,
+    opacity:0
+})
+gsap.from("#clabOne h1",{
+    opacity:0,
+    duration:5,
+    delay:9,
+    opacity:0
+})
+gsap.from("#clabOne p",{
+    opacity:0,
+    duration:5,
+    delay:9,
+    opacity:0
+})
+gsap.from("#clabTwo h1",{
+    opacity:0,
+    duration:5,
+    delay:9,
+    opacity:0
+})
+gsap.from("#clabTwo p",{
+    opacity:0,
+    duration:5,
+    delay:9,
+    opacity:0
+})
+gsap.from("#clabThree h1",{
+    opacity:0,
+    duration:5,
+    delay:9,
+    opacity:0
+})
+gsap.from("#clabThree p",{
+    opacity:0,
+    duration:5,
+    delay:9,
+    opacity:0
+})
+gsap.from("#page3 #op h1 ",{
+    x:-190,
+    opacity:0,
+    duration:4,
+    delay:7
+})
